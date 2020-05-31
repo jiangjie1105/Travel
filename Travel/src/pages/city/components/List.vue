e07ecbcc5b6720c2af3c7f1def31c4f9eafa8b1b
jiangjie1105 <1021404964@qq.com>
2020-05-31 18:50:53 +0800

    zanting

---
+++ Travel/src/pages/city/components/List.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="list">
+    list
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'CityList'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+
+</style>
